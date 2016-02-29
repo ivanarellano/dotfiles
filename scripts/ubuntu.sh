4b9fb52cd5e94c7dad642dd7839bf9696465ac15
Ivan <ia@ivanarellano.com>
2016-02-29 12:17:38 -0500

    Update ubuntu.sh

--- scripts/ubuntu.sh
+++ scripts/ubuntu.sh
@@ -11,8 +11,8 @@ sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo apt-get update
 
 # Essentials, can't run the install scripts without em
-sudo apt-get install -qqy git stow python-setuptools python-dev build-essential python-software-properties
-sudo easy_install pip
+#sudo apt-get install -qqy git stow python-setuptools python-dev build-essential python-software-properties
+#sudo easy_install pip
 
 # Niceties - Integrates well with my oh-my-zsh plugins
 sudo apt-get install -qqy tmux autojump vim-nox
@@ -21,10 +21,10 @@ sudo apt-get install neovim
 sudo pip install neovim
 
 # For the macsters that only make things available via brew
-sudo apt-get install -qqy build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
-ruby -e "$(wget -O- https://raw.github.com/Homebrew/linuxbrew/go/install)"
+#sudo apt-get install -qqy build-essential curl git m4 ruby texinfo libbz2-dev libcurl4-openssl-dev libexpat-dev libncurses-dev zlib1g-dev
+#ruby -e "$(wget -O- https://raw.github.com/Homebrew/linuxbrew/go/install)"
 
-brew install --HEAD hub
+#brew install --HEAD hub
 
 sudo apt-get install -qqy silversearcher-ag
 
